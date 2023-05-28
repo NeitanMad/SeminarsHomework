045a49fa74ce0ee7dd031d98e99e8b3647a579d1
Azerty <Azerty@DESKTOP-HHU9R9S>
2023-05-28 23:10:12 +0300

    Дз 4 готово.

--- Homework1.cs
+++ Homework1.cs
@@ -4,97 +4,100 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("======================ДЗ 1======================");
-            //Console.WriteLine();
-            //// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-            //Console.Write("Введите первое число: ");
-            //int firstNuber = int.Parse(Console.ReadLine());
+            Console.WriteLine("======================ДЗ 1======================");
+            Console.WriteLine();
+            // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+            Console.Write("Введите первое число: ");
+            int firstNuber = int.Parse(Console.ReadLine());
 
-            //Console.Write("Введите второе число: ");
-            //int secondNumber = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int secondNumber = int.Parse(Console.ReadLine());
 
-            //if (firstNuber > secondNumber)
-            //{
-            //    Console.WriteLine($"Число {firstNuber} больше числа {secondNumber}");
-            //}
-            //else if (firstNuber < secondNumber)
-            //{
-            //    Console.WriteLine($"Число {secondNumber} больше числа {firstNuber}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Числа равны!");
-            //}
+            if (firstNuber > secondNumber)
+            {
+                Console.WriteLine($"Число {firstNuber} больше числа {secondNumber}");
+            }
+            else if (firstNuber < secondNumber)
+            {
+                Console.WriteLine($"Число {secondNumber} больше числа {firstNuber}");
+            }
+            else
+            {
+                Console.WriteLine("Числа равны!");
+            }
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-            //Console.WriteLine("Введите три числа");
-            //int[] nubers = new int[3];
-            //int count = 0;
+            // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+            Console.WriteLine("Введите три числа");
+            int[] nubers = new int[3];
+            int count = 0;
 
-            //for (int i = 0; i < nubers.Length; i++)
-            //{
-            //    Console.Write($"{++count} число: ");
-            //    nubers[i] = int.Parse(Console.ReadLine());
-            //}
+            for (int i = 0; i < nubers.Length; i++)
+            {
+                Console.Write($"{++count} число: ");
+                nubers[i] = int.Parse(Console.ReadLine());
+            }
 
-            //Console.WriteLine($"Максимальное из {count} чисел - {nubers.Max()}");
-            //Console.WriteLine();
+            Console.WriteLine($"Максимальное из {count} чисел - {nubers.Max()}");
+            Console.WriteLine();
 
-            //// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+            // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-            //Console.Write("Введите число: ");
-            //int nuber = int.Parse(Console.ReadLine());
+            Console.Write("Введите число: ");
+            int nuber = int.Parse(Console.ReadLine());
 
-            //if (nuber % 2 == 0)
-            //{
-            //    Console.WriteLine(nuber + " четное");
-            //}
-            //else
-            //{
-            //    Console.WriteLine(nuber + " нечетное");
-            //}
-            //Console.WriteLine();
+            if (nuber % 2 == 0)
+            {
+                Console.WriteLine(nuber + " четное");
+            }
+            else
+            {
+                Console.WriteLine(nuber + " нечетное");
+            }
+            Console.WriteLine();
 
-            //// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+            // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-            //Console.Write("Введите число 'N': ");
-            //int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите число 'N': ");
+            int n = int.Parse(Console.ReadLine());
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //Console.Write("Чётные числа от 1 до N: ");
+            Console.Write("Чётные числа от 1 до N: ");
 
-            //for (int i = 1; i <= n; i++)
-            //{
-            //    if (i % 2 == 0)
-            //    {
-            //        if (i + 1 < n)
-            //        {
-            //            Console.Write($"{i}, ");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i + 1 < n)
+                    {
+                        Console.Write($"{i}, ");
 
-            //        }
+                    }
 
 
-            //        else
-            //            Console.Write($"{i}");
-            //    }
-            //}
+                    else
+                        Console.Write($"{i}");
+                }
+            }
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
-            //Console.WriteLine("======================ДЗ 2======================");
-            //Console.WriteLine();
+            Console.WriteLine("======================ДЗ 2======================");
+            Console.WriteLine();
 
-            //Homework2.Homework(); // запуск второй домашки
+            Homework2.Homework(); // запуск второй домашки
 
-            //Console.WriteLine("======================ДЗ 3======================");
-            //Console.WriteLine();
+            Console.WriteLine("======================ДЗ 3======================");
+            Console.WriteLine();
 
-            //Homework3.Homework(); // запуск третьей домашки
+            Homework3.Homework(); // запуск третьей домашки
 
-            Homework4.Homework();
+            Console.WriteLine("======================ДЗ 4======================");
+            Console.WriteLine();
+
+            Homework4.Homework();// запуск четвертой домашки
         }
     }
 }
--- Homework4.cs
+++ Homework4.cs
@@ -4,60 +4,36 @@
     {
         public static void Homework()
         {
-            //// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных
 
-            //Console.Write("Укажите длину массива: ");
-            //int length = int.Parse(Console.ReadLine());
+            // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+            Console.Write("Введите число A: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Введите число B: ");
+            int secondNumber = int.Parse(Console.ReadLine());
 
-            //int[] array = new int[length];
+            int result = 1;
 
-            //Random random = new Random();
+            for (int i = 0; i < secondNumber; i++)
+            {
+                result *= firstNumber;
+            }
 
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    array[i] = random.Next(100, 1000);
-            //}
-
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    if (array[i] % 2 == 0)
-            //    {
-            //        Console.Write($"{array[i]}, ");
-            //    }
-            //}
-
-            //// TODO
-            ///
-
-            //// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-            //Console.Write("Введите число A: ");
-            //int firstNumber = int.Parse(Console.ReadLine());
-            //Console.Write("Введите число B: ");
-            //int secondNumber = int.Parse(Console.ReadLine());
-
-            //int result = 1;
-
-            //for (int i = 0; i < secondNumber; i++)
-            //{
-            //    result *= firstNumber;
-            //}
-
-            //Console.WriteLine("Число A в степени B -> " + result);
-            //Console.WriteLine();
+            Console.WriteLine("Число A в степени B -> " + result);
+            Console.WriteLine();
 
             // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-            //Console.Write("Введите число: ");
-            //string input = Console.ReadLine();
+            Console.Write("Введите число: ");
+            string input = Console.ReadLine();
 
-            //int[] arrayNumbers = new int[input.Length];
+            int[] arrayNumbers = new int[input.Length];
 
-            //for (int i = 0; i < input.Length; i++)
-            //{
-            //    arrayNumbers[i] = Convert.ToInt32(input[i].ToString()); // Приведение к целочисленному значению
-            //}
+            for (int i = 0; i < input.Length; i++)
+            {
+                arrayNumbers[i] = Convert.ToInt32(input[i].ToString()); // Приведение к целочисленному значению
+            }
 
-            //Console.WriteLine("Сумма цифр -> " + arrayNumbers.Sum());
-            //Console.WriteLine();
+            Console.WriteLine("Сумма цифр -> " + arrayNumbers.Sum());
+            Console.WriteLine();
 
             // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
             Console.Write("Введите длину массива: ");
@@ -88,6 +64,7 @@
                     Console.Write(", ");
             }
             Console.Write("]");
+            Console.WriteLine();
 
         }
     }
